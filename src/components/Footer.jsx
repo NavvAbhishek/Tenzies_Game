6fe7ea2c8b2564ab9d47113142c9effe6433bad2
NavvAbhishek <abi2000navi@gmail.com>
2024-10-15 22:27:40 +0530

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <div className="bg-black text-white h-16 flex justify-center items-center">
+      <p className="">
+        Design and Develop by{" "}
+        <a
+          href="https://www.linkedin.com/in/navabhishek/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Navindu Abhishek ❤️
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
